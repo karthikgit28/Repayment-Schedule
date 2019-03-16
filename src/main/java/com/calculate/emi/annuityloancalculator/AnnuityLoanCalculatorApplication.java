3cfa85bb67ae069feba424b9490fbc60e925061d
karthikgit28 <karthik.jec08@gmail.com>
2019-03-16 14:09:52 +0530

    new changes

--- src/main/java/com/calculate/emi/annuityloancalculator/AnnuityLoanCalculatorApplication.java
+++ src/main/java/com/calculate/emi/annuityloancalculator/AnnuityLoanCalculatorApplication.java
@@ -4,19 +4,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.calculate.emi.annuityloancalculator.calculator.LoanCalculator;
+//import com.calculate.emi.annuityloancalculator.calculator.LoanCalculator;
 
 @SpringBootApplication
-public class AnnuityLoanCalculatorApplication implements CommandLineRunner{
+public class AnnuityLoanCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnnuityLoanCalculatorApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		LoanCalculator calculate = new LoanCalculator();
-		calculate.calculateAmount();
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		LoanCalculator calculate = new LoanCalculator();
+//		calculate.calculateAmount();
+//	}
 
 }
